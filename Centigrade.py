cb93165ffd111d7b1128e8d7d92171c44b0d3647
Mohammed Owais <osowais78@gmail.com>
2025-06-15 09:53:16 +0530

    Adding Python Programs.

---
+++ Centigrade.py
@@ -0,0 +1,7 @@
+celsius = float(input("Enter temperature in Centigrade: "))
+fahrenheit = (celsius * 9/5) + 32
+print(celsius, "Centigrade is equal to", fahrenheit)
+if fahrenheit < 32:
+    print("It's freezing!")
+else:
+    print("It's not freezing.")
